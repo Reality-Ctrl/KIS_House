a3cc5d56d90a6bb0b2194d15e78e290b7181694e
Mixail <mixail.korchikov@mail.ru>
2021-04-28 17:38:09 +0300

    SteamVR

--- Assets/Scripts/DoubleDoor.cs
+++ Assets/Scripts/DoubleDoor.cs
@@ -1,22 +1,34 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider))]
 public class DoubleDoor : MonoBehaviour
 {
     private Animator animator;
+    private BoxCollider doorBoxCollider;
 
     private void Awake()
     {
         animator = gameObject.GetComponent<Animator>();
+        doorBoxCollider = gameObject.GetComponent<BoxCollider>();
     }
 
     public void Open()
     {
         animator.SetBool("isOpen", true);
+        doorBoxCollider.enabled = false;
     }
 
     public void Close()
     {
         animator.SetBool("isOpen", false);
+        doorBoxCollider.enabled = true;
+    }
+
+    public void ChangeState()
+    {
+        bool currStatus = animator.GetBool("isOpen");
+        animator.SetBool("isOpen", !currStatus);
+        doorBoxCollider.enabled = currStatus;
     }
 }
